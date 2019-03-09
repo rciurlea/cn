0f229bd17c28bc2e0d2e8ec3fa537c89ebbb9e9b
Radu Ciurlea <raduciurlea@gmail.com>
2019-03-10 00:26:43 +0200

    gauss jordan inversion

--- mat/inverse.go
+++ mat/inverse.go
@@ -1,8 +1,46 @@
 package mat
 
+import "fmt"
+
 // GaussJordan atempts to invert a square matrix A using
 // the Gauss-Jordan method. Panics if A is not square and
 // returns an error if it's not invertible.
 func GaussJordan(A *M) (*M, error) {
-	return nil, nil
+	if A.rows != A.cols {
+		panic("matrix not square")
+	}
+	A = A.Augment(Eye(A.rows))
+	// create zeroes below diagonal, set diagonal to 1
+	for i := 1; i <= A.rows; i++ {
+		pivot := A.Get(i, i)
+		if almostEqual(pivot, 0) {
+			return nil, fmt.Errorf("matrix not invertible")
+		}
+		// set pivot to 1
+		f := 1 / pivot
+		A.Set(i, i, 1)
+		// scale current row
+		for j := i + 1; j <= A.cols; j++ {
+			A.Set(i, j, A.Get(i, j)*f)
+		}
+		// create zeroes under pivot
+		for j := i + 1; j <= A.rows; j++ {
+			f = A.Get(j, i)
+			A.Set(j, i, 0)
+			for k := i + 1; k <= A.cols; k++ {
+				A.Set(j, k, A.Get(j, k)-f*A.Get(i, k))
+			}
+		}
+	}
+	// create zeroes above diagonal
+	for i := A.rows; i >= 1; i-- {
+		for j := i - 1; j >= 1; j-- {
+			f := A.Get(j, i)
+			A.Set(j, i, 0)
+			for k := i + 1; k <= A.cols; k++ {
+				A.Set(j, k, A.Get(j, k)-f*A.Get(i, k))
+			}
+		}
+	}
+	return A.Slice(1, A.cols/2+1, A.rows, A.cols), nil
 }
--- mat/inverse_test.go
+++ mat/inverse_test.go
@@ -1,1 +1,24 @@
 package mat
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGaussJordan(t *testing.T) {
+	m := New(3, 3, 1, 2, 3, 2, 5, 3, 1, 0, 8)
+	fmt.Println(m)
+	n, err := GaussJordan(m)
+	assert.NoError(t, err)
+	assert.True(t, n.Equals(New(3, 3, -40, 16, 9, 13, -5, -3, 5, -2, -1)))
+	assert.True(t, m.Mul(n).Equals(Eye(3)))
+	assert.True(t, n.Mul(m).Equals(Eye(3)))
+}
+
+func TestGaussJordanError(t *testing.T) {
+	m := New(3, 3, 1, 2, 3, 4, 5, 6, 2, 4, 6)
+	m, err := GaussJordan(m)
+	assert.Error(t, err)
+}
